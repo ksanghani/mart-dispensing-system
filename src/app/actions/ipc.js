c112bdeb0706afe2694335cd91257960ce74c3f4
Mario Tacke <mario.tacke@with.me>
2016-09-07 13:11:27 -0700

    (fix) Rename barcode event binding

--- src/app/actions/ipc.js
+++ src/app/actions/ipc.js
@@ -1,7 +1,7 @@
 import { scanBarcode, dispenseItem } from 'reducers/peripherals';
 
 export default {
-    'sensors:barcode:data': (event, barcode) => scanBarcode(barcode),
+    'peripherals:barcode:data': (event, barcode) => scanBarcode(barcode),
     'sds:dispense:success': () => dispenseItem(true),
     'sds:dispense:failure': () => dispenseItem(false)
 };
