3365465589256f699c49df7985b09d99f55c0a9b
Mario Tacke <mario.tacke@with.me>
2016-09-06 17:13:51 -0700

    (refactor) Remove prototype code from main app

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
 process.env.NODE_CONFIG_DIR = `${__dirname}/config`;
 process.env.NODE_ENV = require('./package.json').env || '';
 
-const PeripheralClient = require('wm-peripheral-client');
-const SdsClient        = require('wm-sds-client');
-const debug            = require('debug')('fixture:sds');
-const electron         = require('electron');
-const app              = electron.app;
-const BrowserWindow    = electron.BrowserWindow;
-const ipc              = electron.ipcMain;
-
-const peripherals = new PeripheralClient({
-    proximity: true,
-    barcode: true
-});
-
-const sds = new SdsClient({
-    connectionString: 'http://localhost:5555/srs'
-});
+const debug                           = require('debug')('fixture:sds');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 let win = null;
 
@@ -35,28 +21,8 @@ app.on('ready', () => {
         global.dirname = __dirname;
     }
 
-    peripherals.on('proximity:in-range', inRange =>
-        win.webContents.send('sensors:proximity:in-range', inRange));
-
-    peripherals.on('barcode:data', barcode =>
-        win.webContents.send('sensors:barcode:data', barcode));
-
-    peripherals.connect();
-
-    sds.on('dispense:success', () =>
-        win.webContents.send('sds:dispense:success'));
-
-    sds.on('dispense:failure', () =>
-        win.webContents.send('sds:dispense:failure'));
-
     win.on('closed', () => {
         win = null;
         app.quit();
     });
-});
-
-ipc.on('sds:dispense:item', (event, door, shelf, divider) => {
-    debug('Request to dispense', door, shelf, divider);
-
-    sds.dispense(door, shelf, divider);
 });
