945b8a68080db15d56075a44aee54d92047fc8a4
Ryan Munger <ryanmunger@gmail.com>
2016-09-13 16:31:13 -0400

    Add new animated svg loader for dispensing screen

--- src/app/components/Loader/Loader.js
+++ src/app/components/Loader/Loader.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Loader.scss';
 
-export const Loader = () => <div className={classes.loader} />;
+export class Loader extends Component {
+    render () {
+        return (
+            <svg className={classes['loader-container']} width="238" height="149" viewBox="0 0 672 318">
+                <defs>
+                    <linearGradient id="loaderBodyGradient" x1="100%" y1="100%">
+                        <stop className={classes.loaderBodyGradientStop1} offset="0%"/>
+                        <stop className={classes.loaderBodyGradientStop2} offset="100%"/>
+                    </linearGradient>
+                </defs>
+                <path d="M0 302 129 302 129 17 232 17 232 302 336 302 336 17 440 17 440 302 545 302 545 17 772 17" fill="none" stroke="#f4f4f4" strokeMiterlimit="10" strokeWidth="34" className={classes['loader-body-path']}></path>
+                <path d="M0 302 129 302 129 17 232 17 232 302 336 302 336 17 440 17 440 302 545 302 545 17 772 17" fill="none" stroke='url(#loaderBodyGradient)' strokeMiterlimit="10" strokeWidth="34" className={classes['loader-body']}></path>
+            </svg>
+        );
+    }
+}
 
 export default Loader;
