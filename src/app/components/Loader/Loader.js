d74e0aca87a29be4cbf0a4f0ca596a2128650ee0
Ryan Munger <ryanmunger@gmail.com>
2016-09-15 15:28:55 -0400

    Fine tune loader animation speed and color

--- src/app/components/Loader/Loader.js
+++ src/app/components/Loader/Loader.js
@@ -11,15 +11,9 @@ export class Loader extends Component {
         const loaderContainerClasses = `${classes['loader-container']} ${classes['loader-container-fade-in']}`;
         return (
             <div className={loaderContainerClasses}>
-                <svg style={style} width="119" height="74" viewBox="0 0 672 318">
-                    <defs>
-                        <linearGradient id="loaderBodyGradient" x1="100%" y1="100%">
-                            <stop className={classes.loaderBodyGradientStop1} offset="0%"/>
-                            <stop className={classes.loaderBodyGradientStop2} offset="100%"/>
-                        </linearGradient>
-                    </defs>
-                    <path d="M0 302 129 302 129 17 232 17 232 302 336 302 336 17 440 17 440 302 545 302 545 17 772 17" fill="none" stroke="#f4f4f4" strokeMiterlimit="10" strokeWidth="34"></path>
-                    <path d="M0 302 129 302 129 17 232 17 232 302 336 302 336 17 440 17 440 302 545 302 545 17 772 17" fill="none" stroke='url(#loaderBodyGradient)' strokeMiterlimit="10" strokeWidth="34" className={classes['loader-body']}></path>
+                <svg style={style} width="149" height="93" viewBox="0 0 883 638">
+                    <path d="M0 622 129 622 129 17 232 17 232 622 336 622 336 17 440 17 440 622 545 622 545 17 650 17 650 622 750 622 750 17 883 17" fill="none" stroke="#f4f4f4" strokeMiterlimit="10" strokeWidth="34"></path>
+                    <path d="M0 622 129 622 129 17 232 17 232 622 336 622 336 17 440 17 440 622 545 622 545 17 650 17 650 622 750 622 750 17 883 17" fill="none" stroke='#000' strokeMiterlimit="10" strokeWidth="34" className={classes['loader-body']}></path>
                 </svg>
             </div>
         );
